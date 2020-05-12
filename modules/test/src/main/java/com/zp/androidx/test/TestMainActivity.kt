78d6af147b28e7a52e176472656dff963ed3cf49
Zhaopan <zhaopan@huobi.com>
2020-05-12 20:03:31 +0800

    fix构建脚本;支持compose.

--- modules/test/src/main/java/com/zp/androidx/test/TestMainActivity.kt
+++ modules/test/src/main/java/com/zp/androidx/test/TestMainActivity.kt
@@ -1,6 +1,11 @@
 package com.zp.androidx.test
 
 import android.os.Bundle
+import androidx.compose.Composable
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
+import androidx.ui.material.MaterialTheme
+import androidx.ui.tooling.preview.Preview
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
 import com.zp.androidx.base.arch.BaseActivity
@@ -24,12 +29,23 @@ class TestMainActivity: BaseActivity() {
         setSwipeBackEnable(false)
         Timber.d("进入Test")
 
-        setContent { mainView()}
+        setContent {
+            MaterialTheme {
+                Greeting("Zhaopan")
+            }
+        }
     }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
 @Preview
 @Composable
-fun mainView() {
-
+fun DefaultPreview() {
+    MaterialTheme {
+        Greeting("Android")
+    }
 }
